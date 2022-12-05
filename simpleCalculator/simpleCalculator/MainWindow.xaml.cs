280c196a44d80726849655845fdbe3520f2f7d54
Patryk Maciak <maciakpatryk32@gmail.com>
2022-12-05 22:04:32 +0100

    sieve 2 option

--- simpleCalculator/simpleCalculator/MainWindow.xaml.cs
+++ simpleCalculator/simpleCalculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevExpress.Utils.CommonDialogs.Internal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,80 +26,96 @@ namespace simpleCalculator
             InitializeComponent();
         }
 
-        /*
-        private void primeNumber(object sender, RoutedEventArgs e)
+        private void prime_number(object sender, RoutedEventArgs e)
         {
-            if(int.TryParse(number.Text, out int num) && num>=0)
+            if (int.TryParse(result.Text, out int num) && num > 0)
             {
-                if(isPrime(num))
-                {
-                    result.Text = isPrime(num).ToString();
-                    lowerPrime(num);
-                }
+                if (isPrime(num))
+                    primeText.Text = isPrime(num).ToString();
                 else
-                {
-                    result.Text = isPrime(num).ToString();
-                }
-            }
-            else
-            {
-                MessageBox.Show("FILL CORRECTLY", "ERROOOR!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    primeText.Text = isPrime(num).ToString();
             }
         }
 
-        // Najmniejszymi liczbami pierwszymi są 2, 3, 5, 7, 11, 13, 17, 19
-        private bool isPrime(int num)
+        private void factor_number(object sender, RoutedEventArgs e)
         {
-            if (num < 2)
-                return false;
-
-            for(int i=2; i<num; i++)
+            if (int.TryParse(result.Text, out int num) && num > 0 && num <= 33)
             {
-                if (num % i == 0) return false;
+                factorText.Text = " ";
+                factorText.Text = factor(num).ToString();
             }
+            else
+                factorText.Text = "CPU has problem..";
+        }
 
+        private void sieve1(object sender, RoutedEventArgs e)
+        {
+            if(int.TryParse(result.Text, out int num) && num>0)
+            {
+                lowerPrimeNumber(num);
+            }
+        }
+
+        private void sieve2(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(result.Text, out int num) && num > 0)
+            {
+                sieveOfEratosthenes(num);
+            }
+        }
+
+        private bool isPrime(int n)
+        {
+            if (n < 2)
+                return false;
+            for (int i = 2; i < n; i++)
+                if (n % i == 0) return false;
             return true;
         }
 
-        private void lowerPrime(int n)
+        private int factor(int n)
         {
-            List<int> primeNumbers = new List<int>();
-            for(int i=2; i<n; i++)
-            {
-                if (isPrime(i))
-                    primeNumbers.Add(i);
-                    
-            }
-
-            smaller.Text = " ";
-    
-            for(int i=0; i<primeNumbers.Count(); i++)
-            {
-                smaller.Text += primeNumbers[i].ToString() + " ";
-            }
-
-        }
-
-        private void factorialNumber(object sender, RoutedEventArgs e)
-        {
-            if(int.TryParse(number.Text, out int num))
-            {
-                result.Text = factorial(num).ToString();
-            }
-        }
-
-        private int factorial(int n)
-        {
-            int i, s;
+            int i,s;
             i = s = 1;
-
-            while(n>=i)
+            while(i<=n)
             {
                 s *= i;
                 i++;
             }
             return s;
         }
-        */
+
+        private void lowerPrimeNumber(int n)
+        {
+            List<int> prime = new List<int>();
+            for (int i = 2; i < n; i++)
+                if (isPrime(i))
+                    prime.Add(i);
+
+            sieveText1.Text = "";
+            for (int i = 0; i < prime.Count(); i++)
+                sieveText1.Text += prime[i].ToString() + " ";
+
+        }
+
+        private void sieveOfEratosthenes(int n)
+        {
+            sieveText2.Text = " ";
+            bool[] prime = new bool[n + 1];
+            for (int i =0; i < n; i++)
+                prime[i] = true;
+
+            for(int p=2; p*p<n; p++)
+            {
+                if (prime[p] == true)
+                    for (int i = p * p; i <= n; i += p)
+                        prime[i] = false;
+            }
+
+            for (int i = 2; i < n; i++)
+                if (prime[i] == true)
+                    sieveText2.Text += i.ToString() + " ";
+        }
+
     }
 }
